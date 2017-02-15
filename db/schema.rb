ef1241a3f7bfb10366f3309c6cf3f10f5fc1fca6
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2017-02-15 16:58:56 +0100

    Rename Model model to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215154324) do
+ActiveRecord::Schema.define(version: 20170215155722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
