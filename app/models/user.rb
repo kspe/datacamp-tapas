748d8b4502eb49075a13c063fc31b8883824963a
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2017-02-15 17:41:56 +0100

    Handle user registration with subscription type selection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  SUBSCRIPTION_TYPES = ['free', 'paid']
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :subscription, inclusion: { in: SUBSCRIPTION_TYPES }
 end
